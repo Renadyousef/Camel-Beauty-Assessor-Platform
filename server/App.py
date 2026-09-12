6adb2e5bd519b67f658c3a710770e9b4843d162b
renad <120721701+Renadyousef@users.noreply.github.com>
2026-09-12 20:11:23 +0300

    Postman testing router done

--- server/App.py
+++ server/App.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-
+from routes.winner_camels_route import router as winner_camels_router
 app = FastAPI()
 
 
 
 #add all routes here and then .use it from routes folder
-
+app.include_router(winner_camels_router)
 
 #server run gate
 # run command is: python -m uvicorn App:app --reload
